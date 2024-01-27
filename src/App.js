7a59e8d541dce612d649257c4bbe8c229c61ce66
perlapantoja7 <perlapantoja210@gmail.com>
2024-01-27 12:26:03 -0600

    actualizacion del 27 enero 2024

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           hola mundo
+           Actualizacion para GIT
         </p>
         <a
           className="App-link"
